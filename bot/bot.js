3bb7ea92099fa58f1aa69939916266188d3a2d51
Reza Aghajani <rezaaghajani@protonmail.com>
2022-02-20 19:42:57 +0330

    Yadambiar message response

--- bot/bot.js
+++ bot/bot.js
@@ -10,8 +10,14 @@ const bot = new TelegramBot(token, {polling: true})
 // start message response
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id 
-    
-    
-    
     bot.sendMessage(chatId, "سلام من قراره بهت یادآوری کنم زیاااد!")
 })
+
+
+// yadambiar message response
+bot.onText(/\/yadambiar (.+)/, (msg,match) => {
+    const chatId = msg.chat.id
+    const resp = match[1]
+    bot.sendMessage(chatId, resp)
+})
+
