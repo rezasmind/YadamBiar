0518950b34da6a150d137d1723a5d0441f52b7e0
Reza Aghajani <rezaaghajani@protonmail.com>
2022-02-20 21:02:47 +0330

    Release

--- bot/bot.js
+++ bot/bot.js
@@ -10,31 +10,62 @@ const bot = new TelegramBot(token, {polling: true})
 // start message response
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id 
-    bot.sendMessage(chatId, "سلام من قراره بهت یادآوری کنم زیاااد!")
-})
+    bot.sendMessage(chatId, "سلام من قراره بهت یادآوری کنم زیاااد!" , {
+        "reply_markup": {
+            "keyboard": [["یادم بیار"]],
+        }
+    })
+
+    });
 
 
 // yadambiar message response
-bot.onText(/\/yadambiar (.+)/, (msg,match) => {
+bot.on("message", (msg,match) => {
     const chatId = msg.chat.id
-    const resp = match[1]
-    const day = resp.split('|')[0]
-    const dayNum = day.split(' ')[0]
-    const hour = resp.split('|')[1]
-    const hourNum = hour.split(' ')[1]
-    const min = resp.split('|')[2]
-    const minNum = min.split(' ')[1]
-    const message = resp.split('|')[3]
-    function getInterval(day,hour,min) {
-        let interval = 0
-        interval = interval + (day * 24 * 60 * 60 * 1000)
-        interval = interval + (hour * 60 * 60 * 1000)
-        interval = interval + (min * 60 * 1000)
-        console.log(interval)
+    var yadambiar = "یادم بیار"
+    var char = "دقیقه"
+    if (msg.text.toString().includes(yadambiar)) {
+        bot.sendMessage(chatId, "خب زمانی که  بهت یاد آوری کنم همراه با پیامشو اینجوری واسم بفرست:  ")
+        bot.sendMessage(chatId, "n روز | x ساعت | y دقیقه | پیام شما")
+        setTimeout(() => {
+            bot.sendMessage(chatId, "n روز | x ساعت | y دقیقه | پیام شما")
+        }, 1000)
+        setTimeout(() => {
+            bot.sendMessage(chatId, "دقیقا متن بالارو کپی کنید و عدد هارو جای گذاری کنید!")
+            bot.sendMessage(chatId, "هرکدوم رو که نمیخواید بجاش عدد 0 بزارید")
+        }, 1000)
+
     }
 
-    setTimeout(() => {
-        bot.sendMessage(chatId, message)
-    }, getInterval(dayNum,hourNum,minNum))
+    if (msg.text.toString().includes(char)) {
+        bot.sendPhoto(msg.chat.id,"./gif.gif",{caption : "پیام دریافت شد ارباب حتما یادم میمونه و بهتون یادآوری میکنم!"} );
+        function remind() {
+            const day = msg.text.split('|')[0]
+            const dayNum = day.split(' ')[0]
+            const hour = msg.text.split('|')[1]
+            const hourNum = hour.split(' ')[1]
+            const min = msg.text.split('|')[2]
+            const minNum = min.split(' ')[1]
+            const message = msg.text.split('|')[3]
+            function getInterval(day,hour,min) {
+                let interval = 0
+                interval = interval + (day * 24 * 60 * 60 * 1000)
+                interval = interval + (hour * 60 * 60 * 1000)
+                interval = interval + (min * 60 * 1000)
+                return interval
+            }
+        
+            setTimeout(() => {
+                bot.sendMessage(chatId, message)
+            }, getInterval(dayNum,hourNum,minNum))
+        }
+
+        setTimeout(() => {
+            remind()
+            }, 2000)
+
+    }
+
+
 })
 
