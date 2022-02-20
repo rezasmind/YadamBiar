73250d9278f784de831535972c56f2a2871a677e
Reza Aghajani <rezaaghajani@protonmail.com>
2022-02-20 20:10:04 +0330

    Create function for calculate miliseconds

--- bot/bot.js
+++ bot/bot.js
@@ -19,8 +19,20 @@ bot.onText(/\/yadambiar (.+)/, (msg,match) => {
     const chatId = msg.chat.id
     const resp = match[1]
     const day = resp.split('|')[0]
+    const dayNum = day.split(' ')[0]
     const hour = resp.split('|')[1]
+    const hourNum = hour.split(' ')[1]
     const min = resp.split('|')[2]
-    console.log(hour)
+    const minNum = min.split(' ')[1]
+    const msg = resp.split('|')[3]
+    function getInterval(day,hour,min) {
+        let interval = 0
+        interval = interval + (day * 24 * 60 * 60 * 1000)
+        interval = interval + (hour * 60 * 60 * 1000)
+        interval = interval + (min * 60 * 1000)
+        console.log(interval)
+    }
+
+
 })
 
